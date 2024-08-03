9aef08e76898d575eca4bccf1978da3544947648
Rashmiiee <118290629+Rashmiiee@users.noreply.github.com>
2024-08-03 11:23:01 +0300

    Update batch_fastqc.sh

--- scripts/batch_fastqc.sh
+++ scripts/batch_fastqc.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+#run fastqc
+
 fastqc -t 6 -o /Users/rashmirao/Documents/Sidra_Externship/RNAseq/RNA_seq_analysis/results/1_fastqc /Users/rashmirao/Documents/Sidra_Externship/RNAseq/RNA_seq_analysis/raw_data/fastq/*.fastq.gz
